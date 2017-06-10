45b089302e3efd21bfeff7d45802166bcc54260e
Татьяна Шур <tshur@webzavod.ru>
2017-06-11 00:47:54 +0300

    форматирование

--- Tutorial1/Tutorial1/Program.cs
+++ Tutorial1/Tutorial1/Program.cs
@@ -3,48 +3,48 @@ using System.Threading;
 
 namespace Tutorial1
 {
-	class Program
-	{
-		/// <summary>
-		/// Переделать код так, чтобы потоки выполнялись один за другим и на момент вывода на экран состояние было "Finished"
-		/// Как можно улучшить этот код? Какие проблемы вы заметили? Улучшайте!
-		/// </summary>
-		/// <param name="args"></param>
-		static void Main(string[] args)
-		{
-			Worker worker = new Worker();
+    class Program
+    {
+        /// <summary>
+        /// Переделать код так, чтобы потоки выполнялись один за другим и на момент вывода на экран состояние было "Finished"
+        /// Как можно улучшить этот код? Какие проблемы вы заметили? Улучшайте!
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
 
-			while (true)
-			{
-				Console.ReadLine();
-				Thread worker1 = new Thread(worker.Start);
-				Thread worker2 = new Thread(worker.Finish);
+            while (true)
+            {
+                Console.ReadLine();
+                Thread worker1 = new Thread(worker.Start);
+                Thread worker2 = new Thread(worker.Finish);
 
                 // блокируем поток main  пока не будут выполнены задачи в потоках
-				worker1.Start();
+                worker1.Start();
                 worker1.Join();
                 //к этому моменту первый поток завершится
-				worker2.Start();
+                worker2.Start();
                 // далее будет соревнование между main и worker2, поэтому ждем завершения worker2
                 worker2.Join();
 
                 Console.WriteLine(worker.Condition);
-			}
-		}
+            }
+        }
 
-		public class Worker
-		{
-			public string Condition = "none";
+        public class Worker
+        {
+            public string Condition = "none";
 
-			public void Start()
-			{
-				Condition = "Just started";
-			}
+            public void Start()
+            {
+                Condition = "Just started";
+            }
 
-			public void Finish()
-			{
-				Condition = "Finished";
-			}
-		}
-	}
+            public void Finish()
+            {
+                Condition = "Finished";
+            }
+        }
+    }
 }
